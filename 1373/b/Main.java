11778afa933486ef209d440ce9bc14e12da05e96
hajime nakamura <h.nakamura0903@gmail.com>
2020-07-07 22:07:53 +0900

    Educational Codeforces Round 90; A-C; ~1600

---
+++ 1373/b/Main.java
@@ -0,0 +1,114 @@
+package b;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+    public void exec() {
+        int t = stdin.nextInt();
+        for (int i = 0; i < t; i++) {
+            String s = stdin.nextString();
+
+            int zero = 0;
+            for (char c : s.toCharArray()) {
+                if (c == '0') {
+                    zero++;
+                }
+            }
+            if (Math.min(zero, s.length() - zero) % 2 == 0) {
+                stdout.println("NET");
+            } else {
+                stdout.println("DA");
+            }
+        }
+    }
+
+    private static final Stdin stdin = new Stdin();
+    private static final Stdout stdout = new Stdout();
+
+    public static void main(String[] args) {
+        new Main().exec();
+        stdout.flush();
+    }
+
+
+    public static class Stdin {
+        private BufferedReader stdin;
+        private Deque<String> tokens;
+        private Pattern delim;
+
+        public Stdin() {
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+            tokens = new ArrayDeque<>();
+            delim = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            try {
+                if (tokens.isEmpty()) {
+                    String line = stdin.readLine();
+                    delim.splitAsStream(line).forEach(tokens::addLast);
+                }
+                return tokens.pollFirst();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout() {
+            stdout =  new PrintWriter(System.out, false);
+        }
+
+        public void printf(String format, Object ... args) {
+            stdout.printf(format, args);
+        }
+
+        public void println(Object ... objs) {
+            String line = Arrays.stream(objs).map(this::deepToString).collect(Collectors.joining(" "));
+            stdout.println(line);
+        }
+
+        private String deepToString(Object o) {
+            if (o == null || !o.getClass().isArray()) {
+                return Objects.toString(o);
+            }
+
+            int len = Array.getLength(o);
+            String[] tokens = new String[len];
+            for (int i = 0; i < len; i++) {
+                tokens[i] = deepToString(Array.get(o, i));
+            }
+            return "{" + String.join(",", tokens) + "}";
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+}
