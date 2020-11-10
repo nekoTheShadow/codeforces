e97753484fc268dc025df2d5cb2b46285c785383
nakamura hajime <h.nakamura0903@gmail.com>
2020-11-10 13:15:47 +0900

    Educational Codeforces Round 97 (Rated for Div. 2); A,B,D; ~1600

---
+++ 1437/d/Main.java
@@ -0,0 +1,213 @@
+package _1437.d;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class Main {
+    public void exec() {
+        int t = stdin.nextInt();
+        while (t-- > 0) {
+            int n = stdin.nextInt();
+            int[] a = stdin.nextIntArray(n);
+            solve(n, a);
+        }
+    }
+
+    public void solve(int n, int[] a) {
+        Map<Integer, List<Integer>> nexts = new HashMap<>();
+        int child = 1;
+        for (int parent = 0; parent < n; parent++) {
+            while (child < n) {
+                nexts.computeIfAbsent(parent, unused -> new ArrayList<Integer>()).add(child);
+                child++;
+                if (child < n && a[child-1] > a[child]) break;
+            }
+        }
+
+        Map<Integer, Integer> depth = new HashMap<>();
+        depth.put(a[0], 1);
+        Deque<Integer> stack = new ArrayDeque<>();
+        stack.add(a[0]);
+        while (!stack.isEmpty()) {
+            int current = stack.removeLast();
+            for (Integer next : nexts.getOrDefault(current, Collections.emptyList())) {
+                depth.put(next, depth.get(current) + 1);
+                stack.addLast(next);
+            }
+        }
+
+        stdout.println(Collections.max(depth.values()));
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout();
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private InputStream in;
+        private byte[] buf;
+        private int ptr;
+        private int len;
+
+        public Stdin(InputStream in) {
+            this.in = in;
+            this.buf = new byte[1024];
+            this.ptr = 0;
+            this.len = 0;
+        }
+
+        public String nextString() {
+            StringBuilder sb = new StringBuilder();
+            byte b;
+            while ((b = read()) != -1) {
+                sb.appendCodePoint(b);
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() {
+            return (int)nextLong();
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            boolean negative = false;
+            int x = 0;
+
+            byte b = read();
+            if (b == '-') {
+                negative = true;
+            } else {
+                x += b-'0';
+            }
+
+            while ((b=read()) != -1) {
+                x *= 10;
+                x += b-'0';
+            }
+
+            return negative ? -x : x;
+        }
+
+        private byte read() {
+            byte b = readByte();
+            if (b == '\r') {
+                readByte(); // LFを読み飛ばす
+                return -1;
+            } else if (b == '\n' || b == ' ') {
+                return -1;
+            } else {
+                return b;
+            }
+        }
+
+        private byte readByte(){
+            if (len == ptr) {
+                try {
+                    ptr = 0;
+                    len = in.read(buf);
+                    if (len == -1) return -1;
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return buf[ptr++];
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout() {
+            stdout =  new PrintWriter(System.out, false);
+        }
+
+        public void printf(String format, Object ... args) {
+            String line = String.format(format, args);
+            if (line.endsWith(System.lineSeparator())) {
+                stdout.print(line);
+            } else {
+                stdout.println(line);
+            }
+        }
+
+        public void println(Object ... objs) {
+            String line = Arrays.stream(objs).map(Objects::toString).collect(Collectors.joining(" "));
+            stdout.println(line);
+        }
+
+        public void printDebug(Object ... objs) {
+            String line = Arrays.stream(objs).map(this::deepToString).collect(Collectors.joining(" "));
+            stdout.printf("DEBUG: %s%n", line);
+            stdout.flush();
+        }
+
+        private String deepToString(Object o) {
+            if (o == null) {
+                return "null";
+            }
+
+            // 配列の場合
+            if (o.getClass().isArray()) {
+                int len = Array.getLength(o);
+                String[] tokens = new String[len];
+                for (int i = 0; i < len; i++) {
+                    tokens[i] = deepToString(Array.get(o, i));
+                }
+                return "{" + String.join(",", tokens) + "}";
+            }
+
+            return Objects.toString(o);
+        }
+
+        private void flush() {
+            stdout.flush();
+        }
+    }
+}
