9eb7e3715ab634d9a7b54560e82eacc35776faf4
hajime nakamura <h.nakamura0903@gmail.com>
2020-12-03 13:25:01 +0900

    Educational Codeforces Round 71 (Rated for Div. 2); A-C; ~1600

---
+++ 1207/c/Main.java
@@ -0,0 +1,215 @@
+package _1207.c;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Objects;
+
+public class Main {
+    public void exec() {
+        int t = stdin.nextInt();
+        while (t-- > 0) {
+            int n = stdin.nextInt();
+            long a = stdin.nextLong();
+            long b = stdin.nextLong();
+            String s = stdin.nextString();
+            f(n, a, b, s);
+        }
+    }
+
+    public void f(int n, long a, long b, String s) {
+        long inf = Long.MAX_VALUE/2 - 1;
+        long[][] dp = new long[2][n+1];
+        for (long[] row : dp) {
+            Arrays.fill(row, inf);
+        }
+        dp[0][0] = b;
+
+        for (int i = 0; i < n; i++) {
+            if (s.charAt(i)=='0') {
+                dp[0][i+1] = Math.min(dp[0][i+1], dp[0][i]+a+b); // そのまま
+                dp[1][i+1] = Math.min(dp[1][i+1], dp[0][i]+2*a+2*b); // あげる
+                dp[1][i+1] = Math.min(dp[1][i+1], dp[1][i]+a+2*b); // そのまま
+                dp[0][i+1] = Math.min(dp[0][i+1], dp[1][i]+2*a+b); // さげる
+            } else {
+                dp[1][i+1] = Math.min(dp[1][i+1], dp[1][i]+a+2*b); // そのまま
+            }
+        }
+        stdout.println(dp[0][n]);
+    }
+
+    private static final Stdin stdin = new Stdin(System.in);
+    private static final Stdout stdout = new Stdout(System.out);
+    private static final Stderr stderr = new Stderr(System.err, true);
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    // ASCII ONLY
+    public static class Stdin {
+        private InputStream in;
+        private byte[] buf;
+        private int ptr;
+        private int len;
+
+        public Stdin(InputStream in) {
+            this.in = in;
+            this.buf = new byte[1024];
+            this.ptr = 0;
+            this.len = 0;
+        }
+
+        public String nextString() {
+            StringBuilder sb = new StringBuilder();
+            byte b;
+            while ((b = read()) != -1) {
+                sb.appendCodePoint(b);
+            }
+            return sb.toString();
+        }
+
+        public int nextInt() {
+            return (int)nextLong();
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            boolean negative = false;
+            int x = 0;
+
+            byte b = read();
+            if (b == '-') {
+                negative = true;
+            } else {
+                x += b-'0';
+            }
+
+            while ((b=read()) != -1) {
+                x *= 10;
+                x += b-'0';
+            }
+
+            return negative ? -x : x;
+        }
+
+        private byte read() {
+            byte b = readByte();
+            if (b == '\r') {
+                readByte(); // LFを読み飛ばす
+                return -1;
+            } else if (b == '\n' || b == ' ') {
+                return -1;
+            } else {
+                return b;
+            }
+        }
+
+        private byte readByte(){
+            if (len == ptr) {
+                try {
+                    ptr = 0;
+                    len = in.read(buf);
+                    if (len == -1) return -1;
+                } catch (IOException e) {
+                    throw new UncheckedIOException(e);
+                }
+            }
+            return buf[ptr++];
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout(PrintStream stdout) {
+            this.stdout =  new PrintWriter(stdout, false);
+        }
+
+        public void println(Object ... objs) {
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stdout.print(objs[i]);
+                if (i != len-1) stdout.print(' ');
+            }
+            stdout.println();
+        }
+
+        public void flush() {
+            stdout.flush();
+        }
+    }
+
+    public static class Stderr {
+        private PrintWriter stderr;
+        private boolean debug;
+
+        public Stderr(PrintStream stderr, boolean debug) {
+            this.stderr =  new PrintWriter(stderr, false);
+            this.debug = debug;
+        }
+
+        public void println(Object ... objs) {
+            if (!debug) return ;
+
+            stderr.print("DEBUG: ");
+            for (int i = 0, len = objs.length; i < len; i++) {
+                stderr.print(deepToString(objs[i]));
+                if (i != len-1) stderr.print(' ');
+            }
+            stderr.println();
+            stderr.flush();
+        }
+
+        private String deepToString(Object o) {
+            if (o == null) {
+                return "null";
+            }
+
+            // 配列の場合
+            if (o.getClass().isArray()) {
+                int len = Array.getLength(o);
+                String[] tokens = new String[len];
+                for (int i = 0; i < len; i++) {
+                    tokens[i] = deepToString(Array.get(o, i));
+                }
+                return "{" + String.join(", ", tokens) + "}";
+            }
+
+            return Objects.toString(o);
+        }
+    }
+}
