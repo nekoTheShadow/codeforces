466f192d6b6e33b9e2b96cb72352b9f2751197da
hajime nakamura <h.nakamura0903@gmail.com>
2020-12-05 23:41:50 +0900

    Educational Codeforces Round 72 (Rated for Div. 2); A-B; ~1600

---
+++ 1217/b/Main.java
@@ -0,0 +1,189 @@
+package _1217.b;
+import java.io.BufferedReader;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.io.UncheckedIOException;
+import java.lang.reflect.Array;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.Objects;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
+public class Main {
+    public void exec() {
+        int t = stdin.nextInt();
+        while (t-- > 0) {
+            int n = stdin.nextInt();
+            long x = stdin.nextLong();
+            long[] d = new long[n];
+            long[] h = new long[n];
+            for (int i = 0; i < n; i++) {
+                d[i] = stdin.nextLong();
+                h[i] = stdin.nextLong();
+            }
+            stdout.println(f(n, x, d, h));
+        }
+    }
+
+    public class Tuple {
+        private long d;
+        private long h;
+
+        public Tuple(long d, long h) {
+            this.d = d;
+            this.h = h;
+        }
+    }
+
+
+    public long f(int n, long x, long[] d, long[] h) {
+        long maxDiff = Long.MIN_VALUE;
+        long maxD = Long.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            maxDiff = Math.max(maxDiff, d[i]-h[i]);
+            maxD = Math.max(maxD, d[i]);
+        }
+
+        if (x <= maxD) {
+            return 1;
+        } else if (maxDiff <= 0) {
+            return -1;
+        } else {
+            long div = (x-maxD)/maxDiff;
+            long mod = (x-maxD)%maxDiff;
+            if (mod == 0) {
+                return div + 1;
+            } else {
+                return div + 2;
+            }
+        }
+    }
+
+    private static final Stdin stdin = new Stdin();
+    private static final Stdout stdout = new Stdout();
+
+    public static void main(String[] args) {
+        try {
+            new Main().exec();
+        } finally {
+            stdout.flush();
+        }
+    }
+
+    public static class Stdin {
+        private BufferedReader stdin;
+        private Deque<String> tokens;
+        private Pattern delim;
+
+        public Stdin() {
+            stdin = new BufferedReader(new InputStreamReader(System.in));
+            tokens = new ArrayDeque<>();
+            delim = Pattern.compile(" ");
+        }
+
+        public String nextString() {
+            try {
+                if (tokens.isEmpty()) {
+                    String line = stdin.readLine();
+                    if (line == null) {
+                        throw new UncheckedIOException(new EOFException());
+                    }
+                    delim.splitAsStream(line).forEach(tokens::addLast);
+                }
+                return tokens.pollFirst();
+            } catch (IOException e) {
+                throw new UncheckedIOException(e);
+            }
+        }
+
+        public int nextInt() {
+            return Integer.parseInt(nextString());
+        }
+
+        public double nextDouble() {
+            return Double.parseDouble(nextString());
+        }
+
+        public long nextLong() {
+            return Long.parseLong(nextString());
+        }
+
+        public String[] nextStringArray(int n) {
+            String[] a = new String[n];
+            for (int i = 0; i < n; i++) a[i] = nextString();
+            return a;
+        }
+
+        public int[] nextIntArray(int n) {
+            int[] a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = nextInt();
+            return a;
+        }
+
+        public double[] nextDoubleArray(int n) {
+            double[] a = new double[n];
+            for (int i = 0; i < n; i++) a[i] = nextDouble();
+            return a;
+        }
+
+        public long[] nextLongArray(int n) {
+            long[] a = new long[n];
+            for (int i = 0; i < n; i++) a[i] = nextLong();
+            return a;
+        }
+    }
+
+    public static class Stdout {
+        private PrintWriter stdout;
+
+        public Stdout() {
+            stdout =  new PrintWriter(System.out, false);
+        }
+
+        public void printf(String format, Object ... args) {
+            String line = String.format(format, args);
+            if (line.endsWith(System.lineSeparator())) {
+                stdout.print(line);
+            } else {
+                stdout.println(line);
+            }
+        }
+
+        public void println(Object ... objs) {
+            String line = Arrays.stream(objs).map(Objects::toString).collect(Collectors.joining(" "));
+            stdout.println(line);
+        }
+
+        public void printDebug(Object ... objs) {
+            String line = Arrays.stream(objs).map(this::deepToString).collect(Collectors.joining(" "));
+            stdout.printf("DEBUG: %s%n", line);
+            stdout.flush();
+        }
+
+        private String deepToString(Object o) {
+            if (o == null) {
+                return "null";
+            }
+
+            // 配列の場合
+            if (o.getClass().isArray()) {
+                int len = Array.getLength(o);
+                String[] tokens = new String[len];
+                for (int i = 0; i < len; i++) {
+                    tokens[i] = deepToString(Array.get(o, i));
+                }
+                return "{" + String.join(",", tokens) + "}";
+            }
+
+            return Objects.toString(o);
+        }
+
+        private void flush() {
+            stdout.flush();
+        }
+    }
+}
